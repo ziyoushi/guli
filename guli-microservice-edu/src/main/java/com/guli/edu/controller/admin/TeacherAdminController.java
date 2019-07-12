568822cb34e6981f19fdf169d5a9401c4c095420
changchen <doublechang123@163.com>
2019-07-12 16:27:53 +0800

    添加分页查询讲师

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -1,10 +1,13 @@
 package com.guli.edu.controller.admin;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.guli.common.vo.R;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,15 +33,34 @@ public class TeacherAdminController {
     //查询teacher
     @ApiOperation("查询所有讲师列表")
     @GetMapping
-    public List<Teacher> list(){
-        return teacherService.list(null);
+    public R list(){
+        List<Teacher> list = teacherService.list(null);
+        return R.ok().data("items",list);
     }
 
     //逻辑删teacher
     @ApiOperation("根据id删除讲师")
     @DeleteMapping("{id}")
-    public boolean deleteById(@PathVariable String id){
-        return teacherService.removeById(id);
+    public R deleteById(@PathVariable String id){
+        teacherService.removeById(id);
+        return R.ok().message("删除成功");
+    }
+
+    //分页查询
+    @ApiOperation("分页查询讲师")
+    @GetMapping("{pageNum}/{pageSize}")
+    public R pageList(
+            @ApiParam(name = "pageNum", value = "当前页码", required = true)
+            @PathVariable Long pageNum,
+            @ApiParam(name = "pageSize", value = "每页记录数", required = true)
+            @PathVariable Long pageSize){
+
+        Page<Teacher> page = new Page<>(pageNum,pageSize);
+        teacherService.page(page,null);
+        List<Teacher> records = page.getRecords();
+        long total = page.getTotal();
+
+        return R.ok().data("rows",records).data("total",total);
     }
 
 }
