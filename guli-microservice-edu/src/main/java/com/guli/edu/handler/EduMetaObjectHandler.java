320709fd860830ab05b25c0ba86a8c79b484cbce
changchen <doublechang123@163.com>
2019-07-12 14:52:10 +0800

    配置时间自动填充

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/handler/EduMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.guli.edu.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author Administrator
+ * @create 2019-07-12 14:49
+ * 配置时间自动填充
+ */
+@Component
+public class EduMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
