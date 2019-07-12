0e68ee7c8ea2ebadad7a15a86474b30ab90e7e94
changchen <doublechang123@163.com>
2019-07-12 15:09:15 +0800

    配置乐观锁、分页、逻辑删除插件

--- guli-microservice-edu/src/main/java/com/guli/edu/config/MyBatisPlusConfig.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/config/MyBatisPlusConfig.java
@@ -1,6 +1,11 @@
 package com.guli.edu.config;
 
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,5 +17,31 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan("com.guli.edu.mapper")
 public class MyBatisPlusConfig {
+
+
+    /**
+     * 乐观锁插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 逻辑删除插件
+     * @return
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
 
 }
