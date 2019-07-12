5624e50c7699ce132b4336886b3244553c6851fa
changchen <doublechang123@163.com>
2019-07-12 16:14:23 +0800

    添加相应类

---
+++ guli-framework-common/src/main/java/com/guli/common/vo/R.java
@@ -0,0 +1,68 @@
+package com.guli.common.vo;
+
+import com.guli.common.constants.ResultCodeEnum;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ * @create 2019-07-12 15:26
+ */
+@Data
+
+public class R {
+
+    private Boolean success;
+
+    private Integer code;
+
+    private String message;
+
+    private Map<String,Object> data = new HashMap<>();
+
+    private R(){};
+
+    public static R ok(){
+        R r = new R();
+        r.setSuccess(ResultCodeEnum.SUCCESS.getSuccess());
+        r.setCode(ResultCodeEnum.SUCCESS.getCode());
+        r.setMessage(ResultCodeEnum.SUCCESS.getMessage());
+        return r;
+    }
+
+    public static R error(){
+        R r = new R();
+        r.setSuccess(false);
+        r.setCode(20001);
+        r.setMessage("失败");
+        return r;
+    }
+
+    public R message(String message){
+        this.setMessage(message);
+        return this;
+    }
+
+    public R code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+
+    public R success(Boolean success){
+        this.setSuccess(success);
+        return this;
+    }
+
+    public R data(Map<String,Object> map){
+        this.setData(map);
+        return this;
+    }
+
+    public R data(String key,Object value){
+        this.data.put(key,value);
+        return this;
+    }
+
+}
