ee08c372059f3c52646a1482a2f61312b632af96
changchen <doublechang123@163.com>
2019-07-12 11:47:36 +0800

    添加跨域注解

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -4,6 +4,7 @@ package com.guli.edu.controller.admin;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.List;
  * @author changchen
  * @since 2019-07-12
  */
+@CrossOrigin //跨域
 @RestController
 @RequestMapping("/admin/edu/teacher")
 public class TeacherAdminController {
