a3ace1d6da4e666719d76d14171cf200fe9fb98c
changchen <doublechang123@163.com>
2019-07-12 17:00:57 +0800

    添加查询条件接口

--- guli-microservice-edu/src/main/java/com/guli/edu/service/TeacherService.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/TeacherService.java
@@ -1,7 +1,9 @@
 package com.guli.edu.service;
 
-import com.guli.edu.entity.Teacher;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.guli.edu.entity.Teacher;
+import com.guli.edu.query.TeacherQuery;
 
 /**
  * <p>
@@ -12,5 +14,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2019-07-12
  */
 public interface TeacherService extends IService<Teacher> {
+
+    void pageQuery(Page<Teacher> pageParam, TeacherQuery teacherQuery);
 
 }
