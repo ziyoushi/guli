d7e831fd25ef5912d0bf5626cd9ddf50bf4d1c1a
changchen <doublechang123@163.com>
2019-07-12 15:24:26 +0800

    配置swagger注解

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -3,6 +3,8 @@ package com.guli.edu.controller.admin;
 
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import java.util.List;
  * @author changchen
  * @since 2019-07-12
  */
+@Api(value = "讲师管理")
 @CrossOrigin //跨域
 @RestController
 @RequestMapping("/admin/edu/teacher")
@@ -25,12 +28,14 @@ public class TeacherAdminController {
     private TeacherService teacherService;
 
     //查询teacher
+    @ApiOperation("查询所有讲师列表")
     @GetMapping
     public List<Teacher> list(){
         return teacherService.list(null);
     }
 
     //逻辑删teacher
+    @ApiOperation("根据id删除讲师")
     @DeleteMapping("{id}")
     public boolean deleteById(@PathVariable String id){
         return teacherService.removeById(id);
