f4879e47c31e948a323732ef5b485af33f656f3d
changchen <doublechang123@163.com>
2019-07-13 11:02:00 +0800

    配置自定义异常

--- guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
+++ guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.guli.common.handler;
 
+import com.guli.common.constants.ResultCodeEnum;
+import com.guli.common.exception.GuliException;
 import com.guli.common.vo.R;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author Administrator
  * @create 2019-07-13 8:29
  */
+//作为异常通知
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,4 +23,29 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         return R.error();
     }
+
+    //特定异常处理
+    //BadSqlGrammarException
+    @ExceptionHandler(BadSqlGrammarException.class)
+    @ResponseBody
+    public R error(BadSqlGrammarException e){
+        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
+    }
+
+    //HttpMessageNotReadableException
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public R error(HttpMessageNotReadableException e){
+        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
+    }
+
+    @ExceptionHandler(GuliException.class)
+    @ResponseBody
+    public R error(GuliException e){
+        e.printStackTrace();
+        return R.error().code(e.getCode()).message(e.getMessage());
+    }
+
 }
