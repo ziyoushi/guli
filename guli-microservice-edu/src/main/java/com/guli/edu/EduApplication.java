d7423aa9d45c28ac67bc5ecda0e8741bebf03409
changchen <doublechang123@163.com>
2019-07-13 08:33:21 +0800

    程序启动 配置扫描包路径

--- guli-microservice-edu/src/main/java/com/guli/edu/EduApplication.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/EduApplication.java
@@ -2,12 +2,14 @@ package com.guli.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Administrator
  * @create 2019-07-12 11:24
  */
 @SpringBootApplication
+@ComponentScan(basePackages={"com.guli.edu","com.guli.common"})
 public class EduApplication {
 
     public static void main(String[] args) {
