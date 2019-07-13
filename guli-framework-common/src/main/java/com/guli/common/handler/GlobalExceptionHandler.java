9813785fa9ea3b19f923ac0460294001e065f0a4
changchen <doublechang123@163.com>
2019-07-13 08:32:11 +0800

    配置统一异常处理

---
+++ guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.guli.common.handler;
+
+import com.guli.common.vo.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Administrator
+ * @create 2019-07-13 8:29
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error();
+    }
+}
