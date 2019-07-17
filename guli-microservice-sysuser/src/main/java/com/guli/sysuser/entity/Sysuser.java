8b2c6c2e4c39687042e387d92da2e827d2fb66c9
changchen <doublechang123@163.com>
2019-07-17 08:35:07 +0800

    系统用户实体类

---
+++ guli-microservice-sysuser/src/main/java/com/guli/sysuser/entity/Sysuser.java
@@ -0,0 +1,14 @@
+package com.guli.sysuser.entity;
+
+import lombok.Data;
+
+/**
+ * @author Administrator
+ * @create 2019-07-16 19:21
+ */
+@Data
+public class Sysuser {
+    private String id;
+    private String username;
+    private String password;
+}
