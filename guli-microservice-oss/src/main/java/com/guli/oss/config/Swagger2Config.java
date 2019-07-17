012c965ef0212fc9f446ab4541547a8da7141c0a
changchen <doublechang123@163.com>
2019-07-17 20:06:18 +0800

    oss微服务 配置swagger

---
+++ guli-microservice-oss/src/main/java/com/guli/oss/config/Swagger2Config.java
@@ -0,0 +1,42 @@
+package com.guli.oss.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Administrator
+ * @create 2019-07-17 18:45
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket webApiConfig(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+
+    }
+
+    private ApiInfo webApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("文件上传通用服务")
+                .description("本文档描述了文件上传通用服务接口定义")
+                .version("1.0")
+                .contact(new Contact("Changchen", "http://atguigu.com", "doublechang123@163.com"))
+                .build();
+    }
+}
