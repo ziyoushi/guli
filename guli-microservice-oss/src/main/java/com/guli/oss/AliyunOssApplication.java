fb23deb14e67db625a787fa1f47359a5aca8364b
changchen <doublechang123@163.com>
2019-07-17 19:57:22 +0800

    添加oss微服务的 自启类

---
+++ guli-microservice-oss/src/main/java/com/guli/oss/AliyunOssApplication.java
@@ -0,0 +1,15 @@
+package com.guli.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * @create 2019-07-17 16:47
+ */
+@SpringBootApplication
+public class AliyunOssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunOssApplication.class,args);
+    }
+}
