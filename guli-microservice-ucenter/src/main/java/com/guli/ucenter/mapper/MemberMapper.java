83dad6a6f527a121d19d83296854a4ed6b887ee3
changchen <doublechang123@163.com>
2019-07-20 11:01:00 +0800

    ucenter微服务 mapper接口

---
+++ guli-microservice-ucenter/src/main/java/com/guli/ucenter/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package com.guli.ucenter.mapper;
+
+import com.guli.ucenter.entity.Member;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 会员表 Mapper 接口
+ * </p>
+ *
+ * @author changchen
+ * @since 2019-07-20
+ */
+public interface MemberMapper extends BaseMapper<Member> {
+
+}
