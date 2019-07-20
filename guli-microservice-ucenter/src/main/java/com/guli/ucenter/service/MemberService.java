a14373bdd36a8512d2ca028f1e80fa899de11a05
changchen <doublechang123@163.com>
2019-07-20 11:02:59 +0800

    ucenter微服务 用户接口

---
+++ guli-microservice-ucenter/src/main/java/com/guli/ucenter/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.guli.ucenter.service;
+
+import com.guli.ucenter.entity.Member;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 会员表 服务类
+ * </p>
+ *
+ * @author changchen
+ * @since 2019-07-20
+ */
+public interface MemberService extends IService<Member> {
+
+}
