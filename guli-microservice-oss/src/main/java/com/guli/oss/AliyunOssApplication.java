0af40d2f6ffe2bde1ee644bab078f5ee2011ef48
changchen <doublechang123@163.com>
2019-07-21 13:12:22 +0800

    oss微服务 启动类添加注册客户端注解

--- guli-microservice-oss/src/main/java/com/guli/oss/AliyunOssApplication.java
+++ guli-microservice-oss/src/main/java/com/guli/oss/AliyunOssApplication.java
@@ -2,11 +2,13 @@ package com.guli.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Administrator
  * @create 2019-07-17 16:47
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class AliyunOssApplication {
     public static void main(String[] args) {
