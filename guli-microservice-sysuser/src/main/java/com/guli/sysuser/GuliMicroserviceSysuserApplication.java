d0e6a7c75cb7b70c845a0725ca4658a7e4b22399
changchen <doublechang123@163.com>
2019-07-21 13:09:29 +0800

    系统用户微服务 自启类 添加注册服务客户端注解

--- guli-microservice-sysuser/src/main/java/com/guli/sysuser/GuliMicroserviceSysuserApplication.java
+++ guli-microservice-sysuser/src/main/java/com/guli/sysuser/GuliMicroserviceSysuserApplication.java
@@ -3,7 +3,10 @@ package com.guli.sysuser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GuliMicroserviceSysuserApplication {
 
