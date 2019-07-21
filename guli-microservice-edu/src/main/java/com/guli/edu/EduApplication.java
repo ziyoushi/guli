63395ce9a70fbb7e1feb407fd084c390af8b2198
changchen <doublechang123@163.com>
2019-07-21 14:37:30 +0800

    edu微服务 修改 启动类

--- guli-microservice-edu/src/main/java/com/guli/edu/EduApplication.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/EduApplication.java
@@ -2,12 +2,14 @@ package com.guli.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Administrator
  * @create 2019-07-12 11:24
  */
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages={"com.guli.edu","com.guli.common"})
 public class EduApplication {
