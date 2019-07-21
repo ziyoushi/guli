85501a4f7656b46c9f3d253723e00743f8729dea
changchen <doublechang123@163.com>
2019-07-21 14:33:41 +0800

    edu微服务 二级课程分类 vo

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/vo/SubjectVo.java
@@ -0,0 +1,14 @@
+package com.guli.edu.vo;
+
+import lombok.Data;
+
+/**
+ * @author Administrator
+ * @create 2019-07-21 13:35
+ * 二级分类vo对象
+ */
+@Data
+public class SubjectVo {
+    private String id;
+    private String title;
+}
