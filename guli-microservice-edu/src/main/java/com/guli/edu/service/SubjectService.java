acd0f31f99132da41d7ff80ee2327a49d17239b6
changchen <doublechang123@163.com>
2019-07-21 13:47:10 +0800

    edu微服务 添加一级课程分类 二级课程分类嵌套封装数据 接口

--- guli-microservice-edu/src/main/java/com/guli/edu/service/SubjectService.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/SubjectService.java
@@ -1,7 +1,11 @@
 package com.guli.edu.service;
 
-import com.guli.edu.entity.Subject;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.guli.edu.entity.Subject;
+import com.guli.edu.vo.SubjectNestedVo;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2019-07-12
  */
 public interface SubjectService extends IService<Subject> {
+
+    //批量导入Excel数据
+    List<String> batchImport(MultipartFile file) throws Exception;
+
+    //封装一级、二级嵌套数据
+    List<SubjectNestedVo> nestedList();
 
 }
