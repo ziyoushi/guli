7e0f9e139489efe5a9a5d522a981db6b8a9e627d
changchen <doublechang123@163.com>
2019-07-22 19:39:22 +0800

    统计微服务 配置mp

---
+++ guli-microservice-statistics/src/main/java/com/guli/statistics/config/MyBatisPlusConfig.java
@@ -0,0 +1,46 @@
+package com.guli.statistics.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Administrator
+ * @create 2019-07-12 11:26
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.guli.statistics.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * 乐观锁插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 逻辑删除插件
+     * @return
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+}
