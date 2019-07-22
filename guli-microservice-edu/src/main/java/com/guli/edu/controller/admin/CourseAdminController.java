fbeff54a69ce7f1fc76d50cb61325e618e94160a
changchen <doublechang123@163.com>
2019-07-22 21:22:25 +0800

    edu微服务 添加后端controller

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/CourseAdminController.java
@@ -0,0 +1,41 @@
+package com.guli.edu.controller.admin;
+
+
+import com.guli.common.vo.R;
+import com.guli.edu.form.CourseInfoForm;
+import com.guli.edu.service.CourseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 课程 后端控制器
+ * </p>
+ *
+ * @author changchen
+ * @since 2019-07-12
+ */
+@Api(description="课程管理")
+@CrossOrigin //跨域
+@RestController
+@RequestMapping("/admin/edu/course")
+public class CourseAdminController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @ApiOperation(value = "新增课程")
+    @PostMapping("save-course-info")
+    public R saveCourseInfo(
+            @ApiParam(name = "CourseInfoForm", value = "课程基本信息", required = true)
+            @RequestBody CourseInfoForm courseInfoForm){
+
+        String courseId = courseService.saveCourseInfo(courseInfoForm);
+        return R.ok().data("courseId", courseId);
+    }
+
+}
+
