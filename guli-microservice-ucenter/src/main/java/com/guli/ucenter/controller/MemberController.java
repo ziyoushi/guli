56574bce035802f55831dff809bf31dc10d93894
changchen <doublechang123@163.com>
2019-07-22 14:04:09 +0800

    用户中心微服务 controller添加跨域注解

--- guli-microservice-ucenter/src/main/java/com/guli/ucenter/controller/MemberController.java
+++ guli-microservice-ucenter/src/main/java/com/guli/ucenter/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.guli.ucenter.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.guli.common.vo.R;
+import com.guli.ucenter.service.MemberService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -13,9 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author changchen
  * @since 2019-07-20
  */
+@CrossOrigin
 @RestController
-@RequestMapping("/ucenter/member")
+@RequestMapping("/admin/ucenter/member")
 public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @ApiOperation(value = "今日注册数")
+    @GetMapping(value = "count-register/{day}")
+    public R registerCount(
+            @ApiParam(name = "day", value = "统计日期")
+            @PathVariable String day){
+        Integer count = memberService.countRegisterByDay(day);
+        return R.ok().data("countRegister", count);
+    }
 
 }
 
