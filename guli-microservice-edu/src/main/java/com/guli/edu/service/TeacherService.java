5549d5b7afec9c2a234af266634b82e7845921fe
changchen <doublechang123@163.com>
2019-07-23 21:29:35 +0800

    edu微服务 讲师service 添加查询讲师Name接口

--- guli-microservice-edu/src/main/java/com/guli/edu/service/TeacherService.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/TeacherService.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.query.TeacherQuery;
 
+import java.util.List;
+
 /**
  * <p>
  * 讲师 服务类
@@ -17,4 +19,6 @@ public interface TeacherService extends IService<Teacher> {
 
     void pageQuery(Page<Teacher> pageParam, TeacherQuery teacherQuery);
 
+    //没有参数 只查询所有的teacher名和id
+    List<Teacher> getAllTeacherName();
 }
