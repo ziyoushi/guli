fb5a2a8ca70d01a952ddbcb15c49140deb22768c
changchen <doublechang123@163.com>
2019-07-23 21:26:46 +0800

    edu微服务 课程service 添加修改课程接口

--- guli-microservice-edu/src/main/java/com/guli/edu/service/CourseService.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/CourseService.java
@@ -2,6 +2,7 @@ package com.guli.edu.service;
 
 import com.guli.edu.entity.Course;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.guli.edu.form.CourseInfoForm;
 
 /**
  * <p>
@@ -13,4 +14,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface CourseService extends IService<Course> {
 
+    //保存课程和课程详细信息
+    String saveCourseInfo(CourseInfoForm courseInfoForm);
+
+    //根据id数据回显
+    CourseInfoForm getCourseInfoFormById(String id);
+
+    //修改课程信息
+    void updateCourseInfoById(CourseInfoForm courseInfoForm);
 }
