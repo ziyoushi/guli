ab2a5856644034291890699c2029577c236a0056
changchen <doublechang123@163.com>
2019-07-23 18:30:31 +0800

    添加teacher vo用来封装teacherName

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/entity/TeacherVo.java
@@ -0,0 +1,38 @@
+package com.guli.edu.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 讲师
+ * </p>
+ *
+ * @author changchen
+ * @since 2019-07-12
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("edu_teacher")
+@ApiModel(value="Teacher对象", description="讲师")
+public class TeacherVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "讲师ID")
+    @TableId(value = "id", type = IdType.ID_WORKER_STR)
+    private String id;
+
+    @ApiModelProperty(value = "讲师姓名")
+    private String name;
+
+}
