50da91edb024151cd5ad58d4e258dbb99b4d7e9e
changchen <doublechang123@163.com>
2019-07-23 20:39:49 +0800

    edu微服务 课程controller添加根据id查询课程、新增课程

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/CourseAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/CourseAdminController.java
@@ -37,5 +37,15 @@ public class CourseAdminController {
         return R.ok().data("courseId", courseId);
     }
 
-}
+    //根据id查询课程
+    @ApiOperation(value = "根据ID查询课程")
+    @GetMapping("/course-info/{id}")
+    public R getCourseById(
+            @ApiParam(name = "id", value = "课程ID", required = true)
+            @PathVariable String id){
+        CourseInfoForm courseInfoForm = courseService.getCourseInfoFormById(id);
 
+        return R.ok().data("item",courseInfoForm);
+    }
+
+}
