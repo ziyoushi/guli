1e14062f0111377b6c896ded939a90bbe9260395
changchen <doublechang123@163.com>
2019-07-23 19:13:16 +0800

    oss微服务 上传controller修改

--- guli-microservice-oss/src/main/java/com/guli/oss/controller/FileUploadController.java
+++ guli-microservice-oss/src/main/java/com/guli/oss/controller/FileUploadController.java
@@ -2,10 +2,12 @@ package com.guli.oss.controller;
 
 import com.guli.common.vo.R;
 import com.guli.oss.service.FileService;
+import com.guli.oss.utils.ConstantPropertiesUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,7 +33,13 @@ public class FileUploadController {
     @PostMapping("/upload")
     public R upload(
             @ApiParam(name = "file", value = "文件", required = true)
-            @RequestParam("file") MultipartFile file) {
+            @RequestParam("file") MultipartFile file,
+            @ApiParam(name = "host", value = "文件上传路径", required = false)
+            @RequestParam(value = "host", required = false) String host) {
+
+        if (!StringUtils.isEmpty(host)){
+            ConstantPropertiesUtil.FILE_HOST = host;
+        }
 
         String uploadUrl = fileService.upload(file);
         //返回r对象
