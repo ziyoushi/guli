5d5eb0f0296ec4f4a4a583d70cf2b7b51b48623e
changchen <doublechang123@163.com>
2019-07-24 21:17:32 +0800

    vod微服务 添加后端controller

---
+++ guli-microservice-vod/src/main/java/com/guli/vod/controller/admin/VideoAdminController.java
@@ -0,0 +1,18 @@
+package com.guli.vod.controller.admin;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @create 2019-07-24 20:29
+ */
+@Api(description="阿里云视频点播微服务")
+@CrossOrigin //跨域
+@RestController
+@RequestMapping("/admin/vod/video")
+public class VideoAdminController {
+
+}
