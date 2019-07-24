b84bc5df5790aefdb35630b66be77dd31c715709
changchen <doublechang123@163.com>
2019-07-24 21:21:39 +0800

    vod微服务 service实现类

---
+++ guli-microservice-vod/src/main/java/com/guli/vod/service/impl/VideoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.guli.vod.service.impl;
+
+import com.guli.vod.service.VideoService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Administrator
+ * @create 2019-07-24 20:26
+ */
+@Service
+//@Slf4j
+public class VideoServiceImpl implements VideoService {
+
+    @Override
+    public String uploadVideo(MultipartFile file) {
+        return null;
+    }
+}
