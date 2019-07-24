32fe385c3ae3e60e3b8b46f55ecf49a97e0c85c2
changchen <doublechang123@163.com>
2019-07-24 21:27:45 +0800

    edu微服务 课程service添加相应方法

--- guli-microservice-edu/src/main/java/com/guli/edu/service/CourseService.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/CourseService.java
@@ -1,8 +1,10 @@
 package com.guli.edu.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.guli.edu.entity.Course;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.guli.edu.form.CourseInfoForm;
+import com.guli.edu.query.CourseQuery;
 
 /**
  * <p>
@@ -22,4 +24,10 @@ public interface CourseService extends IService<Course> {
 
     //修改课程信息
     void updateCourseInfoById(CourseInfoForm courseInfoForm);
+
+    //分页查询
+    void pageQuery(Page<Course> pageParam, CourseQuery courseQuery);
+
+    //根据id删除课程
+    void removeCourseById(String id);
 }
