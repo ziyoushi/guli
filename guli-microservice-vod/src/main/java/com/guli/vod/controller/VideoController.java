941cd9368498947763107bff8f2317cfd41f4790
changchen <doublechang123@163.com>
2019-07-24 21:16:37 +0800

    vod微服务 添加controller

---
+++ guli-microservice-vod/src/main/java/com/guli/vod/controller/VideoController.java
@@ -0,0 +1,16 @@
+package com.guli.vod.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @create 2019-07-24 20:28
+ */
+@CrossOrigin //跨域
+@RestController
+@RequestMapping("/vod/video")
+public class VideoController {
+
+}
