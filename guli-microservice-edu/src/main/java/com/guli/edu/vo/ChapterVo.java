535cf910be3c8c685756b5d8993164193374beeb
changchen <doublechang123@163.com>
2019-07-25 10:24:06 +0800

    edu微服务 添加章节vo

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/vo/ChapterVo.java
@@ -0,0 +1,23 @@
+package com.guli.edu.vo;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Administrator
+ * @create 2019-07-25 10:22
+ */
+@ApiModel(value = "章节信息")
+@Data
+public class ChapterVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String title;
+    private List<VideoVo> children = new ArrayList<>();
+}
