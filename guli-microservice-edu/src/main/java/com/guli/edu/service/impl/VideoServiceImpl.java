30cb45cbc8436ec1134a0e04b4cb2dff0eefe514
changchen <doublechang123@163.com>
2019-07-25 17:18:12 +0800

    edu微服务 课程视频service实现

--- guli-microservice-edu/src/main/java/com/guli/edu/service/impl/VideoServiceImpl.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/impl/VideoServiceImpl.java
@@ -1,9 +1,11 @@
 package com.guli.edu.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.guli.edu.entity.Video;
+import com.guli.edu.form.VideoInfoForm;
 import com.guli.edu.mapper.VideoMapper;
 import com.guli.edu.service.VideoService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +19,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class VideoServiceImpl extends ServiceImpl<VideoMapper, Video> implements VideoService {
 
+    @Override
+    public void saveVideoInfo(VideoInfoForm videoInfoForm) {
+
+        Video videoVo = new Video();
+        BeanUtils.copyProperties(videoInfoForm,videoVo);
+        this.save(videoVo);
+
+    }
+
+    @Override
+    public VideoInfoForm getVideoInfoFormById(String id) {
+
+        Video video = this.getById(id);
+        VideoInfoForm videoInfoForm = new VideoInfoForm();
+        BeanUtils.copyProperties(video,videoInfoForm);
+
+        return videoInfoForm;
+    }
+
+    @Override
+    public void updateVideoInfoById(VideoInfoForm videoInfoForm) {
+        //保存课时基本信息
+        Video video = new Video();
+        BeanUtils.copyProperties(videoInfoForm, video);
+        this.updateById(video);
+    }
+
+    @Override
+    public void removeVideoById(String id) {
+        baseMapper.deleteById(id);
+    }
 }
