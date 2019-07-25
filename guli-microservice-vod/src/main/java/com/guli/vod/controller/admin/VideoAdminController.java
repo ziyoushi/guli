51ea25faa13a3d818b858925068ba4fdfcb35d94
changchen <doublechang123@163.com>
2019-07-25 19:27:40 +0800

    vod微服务 添加后端controller上传

--- guli-microservice-vod/src/main/java/com/guli/vod/controller/admin/VideoAdminController.java
+++ guli-microservice-vod/src/main/java/com/guli/vod/controller/admin/VideoAdminController.java
@@ -1,9 +1,12 @@
 package com.guli.vod.controller.admin;
 
+import com.guli.common.vo.R;
+import com.guli.vod.service.VideoService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Administrator
@@ -14,5 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/vod/video")
 public class VideoAdminController {
+
+    @Autowired
+    private VideoService videoService;
+
+    @PostMapping("/upload")
+    public R uploadVideo(
+            @ApiParam(name = "file", value = "文件", required = true)
+            @RequestParam("file") MultipartFile file) throws Exception {
+
+        String videoId = videoService.uploadVideo(file);
+        return R.ok().message("视频上传成功").data("videoId", videoId);
+    }
 
 }
