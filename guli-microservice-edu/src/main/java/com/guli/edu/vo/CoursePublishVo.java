fb9cb4090f4c4d057720f82eb04108eb8220e859
changchen <doublechang123@163.com>
2019-07-25 17:14:10 +0800

    edu微服务 课程发布实体类

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/vo/CoursePublishVo.java
@@ -0,0 +1,24 @@
+package com.guli.edu.vo;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Administrator
+ * @create 2019-07-25 15:31
+ */
+@ApiModel(value = "课程发布信息")
+@Data
+public class CoursePublishVo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    private String cover;
+    private Integer lessonNum;
+    private String subjectLevelOne;
+    private String subjectLevelTwo;
+    private String teacherName;
+    private String price;//只用于显示
+}
