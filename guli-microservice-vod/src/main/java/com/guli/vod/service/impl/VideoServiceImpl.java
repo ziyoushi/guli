f1af8f8c23e2b8779538c12d288c6279da5259ee
changchen <doublechang123@163.com>
2019-07-25 19:29:03 +0800

    vod微服务 上传方法实现

--- guli-microservice-vod/src/main/java/com/guli/vod/service/impl/VideoServiceImpl.java
+++ guli-microservice-vod/src/main/java/com/guli/vod/service/impl/VideoServiceImpl.java
@@ -1,8 +1,18 @@
 package com.guli.vod.service.impl;
 
+import com.aliyun.vod.upload.impl.UploadVideoImpl;
+import com.aliyun.vod.upload.req.UploadStreamRequest;
+import com.aliyun.vod.upload.resp.UploadStreamResponse;
+import com.guli.common.constants.ResultCodeEnum;
+import com.guli.common.exception.GuliException;
 import com.guli.vod.service.VideoService;
+import com.guli.vod.util.ConstantPropertiesUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Administrator
@@ -12,8 +22,37 @@ import org.springframework.web.multipart.MultipartFile;
 //@Slf4j
 public class VideoServiceImpl implements VideoService {
 
+    //通过文件流上传文件
     @Override
     public String uploadVideo(MultipartFile file) {
-        return null;
+
+        String fileName = file.getOriginalFilename();
+        String title = fileName.substring(0, fileName.lastIndexOf("."));
+        InputStream inputStream = null;
+        try {
+            inputStream = file.getInputStream();
+
+        } catch (IOException e) {
+            throw new GuliException(ResultCodeEnum.VIDEO_UPLOAD_TOMCAT_ERROR);
+        }
+
+        UploadStreamRequest request = new UploadStreamRequest(
+                ConstantPropertiesUtil.ACCESS_KEY_ID,
+                ConstantPropertiesUtil.ACCESS_KEY_SECRET,
+                title,
+                fileName,
+                inputStream);
+
+        UploadVideoImpl uploader = new UploadVideoImpl();
+        UploadStreamResponse response = uploader.uploadStream(request);
+        String videoId = response.getVideoId();
+
+        if (!response.isSuccess()){
+            if(StringUtils.isEmpty(videoId)){
+                throw new GuliException(ResultCodeEnum.VIDEO_UPLOAD_ALIYUN_ERROR);
+            }
+        }
+
+        return videoId;
     }
 }
