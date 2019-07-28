37c91c7b15bb6319c0596c5f3ee7bdcb0304ddbf
changchen <doublechang123@163.com>
2019-07-28 14:40:57 +0800

    vod微服务 service添加刷新上传凭证、获取视频上传地址和凭证方法

--- guli-microservice-vod/src/main/java/com/guli/vod/service/VideoService.java
+++ guli-microservice-vod/src/main/java/com/guli/vod/service/VideoService.java
@@ -1,5 +1,7 @@
 package com.guli.vod.service;
 
+import com.aliyuncs.vod.model.v20170321.CreateUploadVideoResponse;
+import com.aliyuncs.vod.model.v20170321.RefreshUploadVideoResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -7,5 +9,15 @@ import org.springframework.web.multipart.MultipartFile;
  * @create 2019-07-24 20:26
  */
 public interface VideoService {
+
     String uploadVideo(MultipartFile file);
+
+    void removeVideo(String videoId);
+
+    //创建视频播放对象 前端调用可以直接播放视频
+    //获取视频上传地址和凭证
+    CreateUploadVideoResponse getUploadAuthAndAddress(String title, String fileName);
+
+    //刷新上传凭证
+    RefreshUploadVideoResponse refreshUploadAuthAndAddress(String videoId);
 }
