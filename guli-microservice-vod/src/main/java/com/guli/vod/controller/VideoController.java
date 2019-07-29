ead833c60958809e43b4e46ba9d83b203f4f0e51
changchen <doublechang123@163.com>
2019-07-29 20:26:51 +0800

    vod微服务 前端controller

--- guli-microservice-vod/src/main/java/com/guli/vod/controller/VideoController.java
+++ guli-microservice-vod/src/main/java/com/guli/vod/controller/VideoController.java
@@ -1,16 +1,31 @@
 package com.guli.vod.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.guli.common.vo.R;
+import com.guli.vod.service.VideoService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Administrator
  * @create 2019-07-24 20:28
  */
+@Api(description="阿里云视频点播微服务")
 @CrossOrigin //跨域
 @RestController
 @RequestMapping("/vod/video")
 public class VideoController {
 
+    @Autowired
+    private VideoService videoService;
+
+    @GetMapping("get-play-auth/{videoId}")
+    public R getVideoPlayAuth(@PathVariable("videoId") String videoId) {
+
+        ///得到播放凭证
+        String playAuth = videoService.getVideoPlayAuth(videoId);
+
+        //返回结果
+        return R.ok().message("获取凭证成功").data("playAuth", playAuth);
+    }
 }
