7355a476689ee2a13b546f9b0cb32f455066e0da
changchen <doublechang123@163.com>
2019-07-29 19:57:53 +0800

    edu微服务 讲师service实现分页查询

--- guli-microservice-edu/src/main/java/com/guli/edu/service/impl/TeacherServiceImpl.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/impl/TeacherServiceImpl.java
@@ -2,16 +2,18 @@ package com.guli.edu.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.mapper.TeacherMapper;
 import com.guli.edu.query.TeacherQuery;
 import com.guli.edu.service.TeacherService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -66,5 +68,33 @@ public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> impl
     @Override
     public List<Teacher> getAllTeacherName() {
         return teacherMapper.getAllTeacherName();
+    }
+
+    @Override
+    public Map<String, Object> pageListWeb(Page<Teacher> pageParam) {
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByAsc("sort");
+
+        baseMapper.selectPage(pageParam, queryWrapper);
+
+        Map<String, Object> map = new HashMap<>();
+
+        List<Teacher> records = pageParam.getRecords();
+        long current = pageParam.getCurrent();
+        long pages = pageParam.getPages();
+        long size = pageParam.getSize();
+        long total = pageParam.getTotal();
+        boolean hasNext = pageParam.hasNext();
+        boolean hasPrevious = pageParam.hasPrevious();
+
+        map.put("items", records);
+        map.put("current", current);
+        map.put("pages", pages);
+        map.put("size", size);
+        map.put("total", total);
+        map.put("hasNext", hasNext);
+        map.put("hasPrevious", hasPrevious);
+
+        return map;
     }
 }
