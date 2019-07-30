4504b707c7862a90ce5771ec76e6b1b508b765b4
changchen <doublechang123@163.com>
2019-07-30 19:43:38 +0800

    用户中心 添加jwt工具类

---
+++ guli-microservice-ucenter/src/main/java/com/guli/ucenter/util/JwtUtils.java
@@ -0,0 +1,55 @@
+package com.guli.ucenter.util;
+
+import com.guli.ucenter.entity.Member;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+/**
+ * @author Administrator
+ * @create 2019-07-30 11:48
+ */
+public class JwtUtils {
+    public static final String SUBJECT = "guli-user";
+
+    //秘钥
+    public static final String APPSECRET = "guli666";
+
+    //过期时间，毫秒，30分钟
+    public static final long EXPIRE = 1000 * 60 * 30;
+
+    /**
+     * 生成Jwt令牌
+     * @param member
+     * @return
+     */
+    public static String generateJWT(Member member){
+
+        String token = Jwts.builder()
+                .setSubject(SUBJECT)
+                .claim("id", member.getId())
+                .claim("nickname", member.getNickname())
+                .claim("avatar", member.getAvatar())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
+                .signWith(SignatureAlgorithm.HS256, APPSECRET).compact();
+
+        return token;
+    }
+
+    /**
+     * 校验jwt
+     * @param jwtToken
+     * @return
+     */
+    public static Claims checkJWT(String jwtToken){
+
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(APPSECRET).parseClaimsJws(jwtToken);
+        Claims claims = claimsJws.getBody();
+
+        return claims;
+    }
+}
