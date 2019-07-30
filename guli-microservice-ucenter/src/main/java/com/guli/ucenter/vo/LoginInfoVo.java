08d7252f18308f46942241e7e84cfa1e4fa1906d
changchen <doublechang123@163.com>
2019-07-30 19:42:40 +0800

    用户中心微服务 前端登录vo

---
+++ guli-microservice-ucenter/src/main/java/com/guli/ucenter/vo/LoginInfoVo.java
@@ -0,0 +1,27 @@
+package com.guli.ucenter.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Administrator
+ * @create 2019-07-30 19:30
+ */
+@Data
+@ApiModel(value="用户基本信息")
+public class LoginInfoVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "会员id")
+    private String id;
+
+    @ApiModelProperty(value = "昵称")
+    private String nickname;
+
+    @ApiModelProperty(value = "用户头像")
+    private String avatar;
+}
