5d1d5c37528345a723032bf9b50760e1ce4290d1
changchen <doublechang123@163.com>
2019-07-30 19:48:03 +0800

    用户中心微服务 service添加根据openId查询用户信息 实现

--- guli-microservice-ucenter/src/main/java/com/guli/ucenter/service/impl/MemberServiceImpl.java
+++ guli-microservice-ucenter/src/main/java/com/guli/ucenter/service/impl/MemberServiceImpl.java
@@ -1,9 +1,10 @@
 package com.guli.ucenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.guli.ucenter.entity.Member;
 import com.guli.ucenter.mapper.MemberMapper;
 import com.guli.ucenter.service.MemberService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +18,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberServiceImpl extends ServiceImpl<MemberMapper, Member> implements MemberService {
 
+    @Override
+    public Integer countRegisterByDay(String day) {
+        return baseMapper.selectRegisterCount(day);
+    }
+
+    @Override
+    public Member getByOpenid(String openid) {
+        QueryWrapper<Member> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("openid", openid);
+        return baseMapper.selectOne(queryWrapper);
+    }
 }
